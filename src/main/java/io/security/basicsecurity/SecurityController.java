783ae2b4d5ba162f4d7f81c01b4487c885a372ae
desirel <two_somang@icloud.com>
2021-05-07 06:04:31 +0900

    feat:Form Login 인증

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -10,4 +10,10 @@ public class SecurityController {
     public String index() {
         return "home";
     }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
 }
