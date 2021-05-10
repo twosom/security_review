7b0c356c0e5d5774f5c22d9a865ef5811285094a
desirel <two_somang@icloud.com>
2021-05-10 17:18:02 +0900

    feat: 인가 개념 및 필터 이해

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -13,7 +13,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .anyRequest().authenticated();
+                .antMatchers("/user").hasRole("USER")
+                .anyRequest().permitAll();
 
         http.formLogin();
     }
